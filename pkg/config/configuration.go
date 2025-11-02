6d5da76f4cb1eb31f79f9b2af892ab243d66a3d5
Lorech <laurisdedumets@gmail.com>
2025-11-02 20:58:24 +0200

    feat(config): Implement configuration file parsing
    Parses the configuration file into structured data, currently starting
    with the flag configuration

--- pkg/config/configuration.go
+++ pkg/config/configuration.go
@@ -1,18 +1,23 @@
 package config
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 
 	"github.com/santhosh-tekuri/jsonschema/v6"
 )
 
+type Config struct {
+	Flags FlagConfig `json:"flags"`
+}
+
 const DefaultConfig = "config.json"
 const schema = "schema/config.schema.json"
 
 // Validates a configuration file against the JSON schema.
 //
-// If config is an empty string, the default config file `config.json` will be used.
+// If `config` is an empty string, the default config file `config.json` will be used.
 func Validate(config string) error {
 	c := jsonschema.NewCompiler()
 	sch, err := c.Compile(schema)
@@ -28,6 +33,7 @@ func Validate(config string) error {
 	if err != nil {
 		return fmt.Errorf("could not open config file: %v", err)
 	}
+	defer f.Close()
 
 	inst, err := jsonschema.UnmarshalJSON(f)
 	if err != nil {
@@ -40,4 +46,28 @@ func Validate(config string) error {
 	}
 
 	return nil
+}
+
+// Parses a configuration file into a struct.
+//
+// If `config` is an empty string, the default config file `config.json` will be used.
+func Parse(config string) (Config, error) {
+	var c Config
+
+	if config == "" {
+		config = DefaultConfig
+	}
+
+	f, err := os.Open(config)
+	if err != nil {
+		return c, fmt.Errorf("could not open config file: %v", err)
+	}
+	defer f.Close()
+
+	dec := json.NewDecoder(f)
+	if err := dec.Decode(&c); err != nil {
+		return c, fmt.Errorf("could not parse config file: %v", err)
+	}
+
+	return c, nil
 }
